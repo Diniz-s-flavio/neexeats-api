9a07e3ef575022eddf9dfbe30d43226773572c4b
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2023-04-11 09:39:44 -0300

    Testes de Integração(Erro)
    Erro ao executar os testes pelo Maven

--- src/test/java/com/nxstage/neexeatsapi/NeeXEatsApiApplicationTests.java
+++ src/test/java/com/nxstage/neexeatsapi/NeeXEatsApiApplicationTests.java
@@ -1,13 +1,62 @@
 package com.nxstage.neexeatsapi;
 
+import com.nxstage.neexeatsapi.domain.exception.CozinhaNaoEncontradaException;
+import com.nxstage.neexeatsapi.domain.exception.EntidadeEmUsoException;
+import com.nxstage.neexeatsapi.domain.model.Kitchen;
+import com.nxstage.neexeatsapi.domain.service.CadastroCozinhaService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import javax.validation.ConstraintViolationException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-class NeeXEatsApiApplicationTests {
+class CadastroCozinhaIT {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
 
 	@Test
-	void contextLoads() {
+	public void whenCadastroCozinhaComDadosCorretos_ThenDeveAtribuirId(){
+		//cenário
+		Kitchen kitchen =  new Kitchen();
+		kitchen.setNome("Chinesa");
+
+		//ação
+		kitchen = cadastroCozinha.salvar(kitchen);
+
+		//validação
+		assertThat(kitchen).isNotNull();
+		assertThat(kitchen.getId()).isNotNull();
+	}
+
+	@Test
+	public void shouldFalhar_whenCozinhaSemNome(){
+		Kitchen kitchen =  new Kitchen();
+		kitchen.setNome(null);
+		ConstraintViolationException erroEsperado =
+				Assertions.assertThrows(ConstraintViolationException.class, () -> {
+					cadastroCozinha.salvar(kitchen);
+				});
+
+	}
+
+	@Test
+	public void shouldFalhar_whenDeletarCozinhaInexistente(){
+		Long id = 999L;
+		CozinhaNaoEncontradaException erroEspesrado =
+				Assertions.assertThrows(CozinhaNaoEncontradaException.class, ()->{
+					cadastroCozinha.excluir(id);
+				});
+	}
+
+	@Test
+	public void ShouFalhar_WhenDeletarCozinhaEmUso(){
+		Long id = 1L;
+		EntidadeEmUsoException erroEsperado =
+				Assertions.assertThrows(EntidadeEmUsoException.class, ()->
+						cadastroCozinha.excluir(id));
 	}
 
 }
