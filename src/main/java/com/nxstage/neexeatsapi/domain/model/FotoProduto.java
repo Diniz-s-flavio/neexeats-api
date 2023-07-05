ecc0cd727f91c7b818cea9271cf7da0e1e265448
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2023-07-05 17:40:40 -0300

    update 05-07-2023

---
+++ src/main/java/com/nxstage/neexeatsapi/domain/model/FotoProduto.java
@@ -0,0 +1,26 @@
+package com.nxstage.neexeatsapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+public class FotoProduto {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "produto_id")
+    private Long id;
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Produto produto;
+    private String nomeArquivo;
+    private String descricao;
+    private String contentType;
+    private Long tamanho;
+}
