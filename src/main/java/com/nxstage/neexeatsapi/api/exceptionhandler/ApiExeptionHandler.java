4ab2d782ef5c801ddced7d35fdc4ce28b6291444
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2023-07-14 18:01:41 -0300

    Update ApiExeptionHandler.java

--- src/main/java/com/nxstage/neexeatsapi/api/exceptionhandler/ApiExeptionHandler.java
+++ src/main/java/com/nxstage/neexeatsapi/api/exceptionhandler/ApiExeptionHandler.java
@@ -20,6 +20,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,6 +41,12 @@ public class ApiExeptionHandler  extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.status(status).headers(headers).build();
+    }
 
     @ExceptionHandler(FileUploadException.class)
     public ResponseEntity<?> fileUploadException(FileUploadException ex, WebRequest request) {
